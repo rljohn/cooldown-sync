16c14ddbd40c6821f1d6055982861ce0765e9895
rljohn <rljohnmoore@gmail.com>
2023-06-24 21:54:02 -0400

    update package script

--- package.py
+++ package.py
@@ -1,57 +1,67 @@
 import shutil
 import sys
 import os
+from distutils.dir_util import copy_tree
 
-# Get the directory path of the current Python file
-current_dir = os.path.dirname(os.path.abspath(__file__))
-os.chdir(current_dir)
+version = "1.1"
+addon_name = "CooldownSync"
+staging_folder_name = "build"
 
-# version
-name="CooldownSync"
-major=1
-minor=0
-version=""
+def create_staging_folder(folder_name):
+    if not os.path.exists(folder_name):
+        # Create the new folder
+        os.mkdir(folder_name)
+        print(f"Folder '{folder_name}' created successfully.")
+    else:
+        print(f"Folder '{folder_name}' already exists.")
 
-# read in version.txt
-file_name = "version.txt"
-try:
-    with open(file_name, "r") as file:
-        lines = file.readlines()
+def cleanup_staging_folder(folder_name):
+    shutil.rmtree(folder_name)
 
-    # parse out major/minor
-    for line in lines:
-        if line.startswith("MAJOR="):
-            major = int(line.split("=")[1])
-        elif line.startswith("MINOR="):
-            minor = int(line.split("=")[1])
-except:
-    print('Could not read version from file')
+def copy_to_staging_folder(addon_name, folder_name):
+    outdir = os.path.join(folder_name, addon_name)
+    copy_tree(addon_name, outdir)
 
-# increment minor and update version string
-minor = minor + 1
-version = f"{major}.{minor}"
-
-# write back to file
-with open(file_name, "w") as file:
-    file.write(f"MAJOR={major}\n")
-    file.write(f"MINOR={minor}\n")
-
-# update version in the config panel
-file_name = "CooldownSync/config_panels.lua"
-
-# Read the contents of the file
-try:
-    with open(file_name, "r") as file:
-        file_contents = file.read()
-
-    # replace version
-    modified_contents = file_contents.replace("<VERSION>", version)
-
-    # Write the modified contents back to the file
-    with open(file_name, "w") as file:
-        file.write(modified_contents)
-except:
-    print('Failed to update UI file')
+def update_version(addon_name, folder_name, version):
     
-zip_file = f"{name}_{version}_Release"
-shutil.make_archive(zip_file, 'zip', base_dir=name)
+    # Read the contents of the file
+    try:
+        file_name = os.path.join(addon_name, addon_name, "config_panels.lua")
+        with open(file_name, "r") as file:
+            file_contents = file.read()
+
+        # replace version
+        modified_contents = file_contents.replace("<VERSION>", version)
+
+        # Write the modified contents back to the file
+        with open(file_name, "w") as file:
+            file.write(modified_contents)
+    except:
+        print('failed to update version')
+
+def main():
+
+    try:
+
+        # ensure local path is set
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(current_dir)
+
+        # copy files to staging folder
+        create_staging_folder(staging_folder_name)
+        copy_to_staging_folder(addon_name, staging_folder_name)
+
+        # remove existing zip file
+        zip_file = f"{addon_name}_{version}_Release"
+        os.remove(zip_file)
+
+        # generate new zip file
+        shutil.make_archive(zip_file, 'zip', base_dir=os.path.join(staging_folder_name, addon_name))
+    except:
+        pass
+    finally:
+        cleanup_staging_folder(staging_folder_name)
+
+if __name__ == "__main__":
+    main()
+
